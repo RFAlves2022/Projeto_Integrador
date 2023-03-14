3390eba3f97518d09fcca39312a2cf756ed7d5e6
RFAlves2022 <94083934+RFAlves2022@users.noreply.github.com>
2023-03-14 17:11:24 -0300

    .

--- header.php
+++ header.php
@@ -8,4 +8,11 @@
     <link rel="stylesheet" href="style.css">
     <title></title>
 </head>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Inicio</a>
+    </div>
+</nav>
+
 <body>
