ddfa127606227a6f7168489c4f9e0491e4c5626c
RFAlves2022 <94083934+RFAlves2022@users.noreply.github.com>
2023-03-20 22:21:07 -0300

    .

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ echo '<main class="container-fluid">';
 echo '<div class="row">';
 while($umTitulo = mysqli_fetch_assoc($arrayTitulos)){
     echo "<div class='col-2 h-100'>";
-    echo '<a class="text-decoration-none link-light" href="">';
+    echo '<a class="text-decoration-none link-light" href="pagina-filme.php?id='. $umTitulo['id'] .'">';
     echo    '<div class="card bg-dark text-white mt-2 mb-2">';
     echo        '<img src="img/'.$umTitulo['img'].' " class="imgsize" class="card-img-top imagem" alt="Imagem do titulo" >';
     echo        '<div class="card-body">';
