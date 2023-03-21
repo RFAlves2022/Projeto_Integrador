5b39679a777eb36e029fa0844f1817508937e700
RFAlves2022 <94083934+RFAlves2022@users.noreply.github.com>
2023-03-21 15:50:42 -0300

    finalizaçao do index

--- footer.php
+++ footer.php
@@ -1,13 +1,12 @@
-</body>
-
-<footer class="bg-dark text-center text-lg-start"> 
+<footer class="bg-dark text-center text-lg-start">
   <!-- Copyright -->
   <div class="text-center text-white p-2">
-    © 2023 Copyright:&nbsp; Desenvolvido por 
+    © 2023 Copyright:&nbsp; Desenvolvido por
     <a class="text-white" href="#!">Rafael Alves</a>
   </div>
   <!-- Copyright -->
 </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
 </html>
